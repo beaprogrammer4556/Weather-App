9d91a62b4724bd57944a332bfb6f7c40582365c5
Saikat <saikatdeybst@gmail.com>
2023-10-23 21:20:05 +0530

    first commt

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
+          Weather App
+        </a>{" "}
+         {" | "}Developed by{" "}
+        <a target="_blank" href="https://www.gauravghai.dev/">
+          Saikat Dey
+        </a>{" "}
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "6f1aca29439fdcd4bdd7b2bac98fdba7",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
